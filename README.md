7e7a625029f3c8852c95670b31f6b99ca47127f8
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2017-02-28 16:03:55 -0600

    Add category pages
    Create the category pages and link each CSV-generated page to its
    categories.  Add a note about troubleshooting this to README.md.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ots-tools
+
+
+# Troubleshooting note for csv2wiki
+
+I had to run
+
+    $ sudo apt-get install php7.0-mysql
+
+to get MediaWiki's rebuildall.php script to run.  I needed that script
+to make my categories work.
+
+
+Sources for this solution:
+
+- [Categories aren't working.](https://www.mediawiki.org/wiki/Topic:T6uzpn51mgb8n5sc)
+- [Database connection fails.](https://www.mediawiki.org/wiki/Thread:Project:Support_desk/MediaWiki_upgrade_fails_with_Database_error/reply)
+
+
+Note that the rebuildall.php script seems slow but takes less than 10
+minutes (for me, on a small wiki, at any rate).
--- csv2wiki.py
+++ csv2wiki.py
@@ -1,8 +1,12 @@
 # Parses a CSV and transforms each line into a MediaWiki page.  This is
 # a WIP.
 #
+# The site name argument should look something like
+# "localhost/mediawiki".  We specify 'http' by default because localhost
+# doesn't support HTTPS.
+#
 # Usage:
-# $ python csv2wiki.py <filename> <site> <username> <password>
+# $ python csv2wiki.py <filename> <site name> <username> <password>
 
 import csv
 from mwclient import Site
@@ -14,6 +18,7 @@ site.login(sys.argv[3], sys.argv[4])
 
 toc_page = site.pages['List of Proposals']
 toc_text = ""
+categories = []
 
 # read in csv
 with open(csv_file, 'rb') as csvfile:
@@ -40,23 +45,43 @@ with open(csv_file, 'rb') as csvfile:
             cell_num = 0
             for cell in row:
                 if cell_num == 0:
-                    toc_text += cell
+                    toc_text += cell + "\n"
                 # Set the contents of each cell to their own section.
                 if cell is not "":
                     # A section can only be created with some text
                     # 
+                    if cell_num == (len(row) - 1):
+                        # For the last column, create a category (NOTE:
+                        # this is overly customized to a certain set of
+                        # CSVs; feel free to remove this conditional for
+                        # other CSVs)
+                        cell_text = '[[Category:' + cell + ']]'
+                        
+                        # Add this to the list of categories, unless
+                        # it's already there:
+                        if cell not in categories:
+                            categories.append(cell)
+                    else:
+                        cell_text = cell
                     # TODO: it's probably bad practice to save each page
                     # many times, and it's definitely slowing down the
                     # script.
                     try:
-                        page.save(cell, section=cell_num, sectiontitle=header_array[cell_num])
+                        page.save(cell_text, section=cell_num, sectiontitle=header_array[cell_num])
                     except:
-                        page.save(cell, section='new', sectiontitle=header_array[cell_num])
+                        page.save(cell_text, section='new', sectiontitle=header_array[cell_num])
                     
-                # TODO: set certain cells as categories instead of sections
                 cell_num += 1
 
         row_num += 1
         
 # create the TOC page.
 toc_page.save(toc_text)
+
+# generate the category pages
+for category in categories:
+    print(category)
+    page_title = 'Category:' + category
+    page = site.pages[page_title]
+    page.save("")
+
