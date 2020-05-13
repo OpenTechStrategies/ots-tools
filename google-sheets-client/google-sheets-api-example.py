b2e295189dc6321da46bf794157cd2ec4f78a9f1
Karl Fogel <kfogel@red-bean.com>
2020-05-13 16:20:50 -0500

    Put an explicit libre copyright on everything
    James and I agreed that we should be doing this is a best practice,
    no matter how short the program.

--- google-sheets-client/google-sheets-api-example.py
+++ google-sheets-client/google-sheets-api-example.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 #
-# Copyright 2018 Open Tech Strategies
-#
 # Based on https://developers.google.com/sheets/api/quickstart/python
 # and https://gist.github.com/burnash/6771295, mashed together into this
 # ungodly monster, yea verily I, like Frankenstein, am loath to look
@@ -11,6 +9,23 @@
 # https://github.com/gsuitedevs/python-samples/blob/4acb66239db78090eacd1863afbd060a4438462a/sheets/quickstart/quickstart.py#L16-L45),
 # so that's probably the right choice for this script as well.
 #
+# -------------------------------------------------------------------------
+# Copyright (C) 2018 Open Tech Strategies, LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------
+# 
 # Get the dependencies:
 #
 # pip install --upgrade google-api-python-client
--- validate-authz.py
+++ validate-authz.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2019 Open Tech Strategies, LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 """Validate authz file by making sure the paths added in the currend
 diff and/or last commit are actual paths that exist in our checkout.
 If it finds any errors, it prints those paths.  Otherwise, it exists
